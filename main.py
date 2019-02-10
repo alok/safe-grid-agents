be1ac6ff2aa4544a2ea80286f5fdc459c28eaa7f
Alok Singh <alokbeniwal@gmail.com>
2019-02-09 19:22:30 -0800

    Make Tune opt in with more explicit API
    Tunable parameters have to be set explicitly, but doing so is fairly
    lightweight since you can just do it with the `-t` flag for each one.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import subprocess
 
 import ray
 from ray import tune
+
 from safe_grid_agents.parsing import prepare_parser
 from train import train
 from tune_config import tune_config
@@ -20,14 +21,15 @@ args.commit_id = (
     .strip()
 )
 
+
 ######## Logging into TensorboardX ########
 # If `args.log_dir` is None, we use the TensorBoardX default.
 if not (args.log_dir is None or os.path.exists(args.log_dir)):
     os.makedirs(args.log_dir, exist_ok=True)
 
-if args.tune:
-    ray.init()
 
+if args.tune is not None:
+    ray.init()
     config = tune_config(args)
 
     # This lets us use argparse on top of ray tune while conforming
--- tune_config.py
+++ tune_config.py
@@ -1,53 +1,41 @@
 import argparse
 import random
-from typing import Any, Callable, Dict, Sequence, Union
+from copy import deepcopy
+from typing import Any, Dict
 
 from ray import tune
 
 
-def config_from_argparse(
-    args: argparse.Namespace, argparse_attr: str, tune_opts: Sequence[float]
-) -> Union[float, Callable[[Any], float]]:
-    """Helper function to decide whether to use argparse or Ray Tune for a
-    hyperparameter.
+TUNE_REQUIRED_CONFIG = {"num_samples": 1}
 
-    Calling `config_from_argparse('lr', ...) would look for `args.lr`,
-    and if it exists and is not None, uses it. Otherwise it uses Ray
-    Tune.
-
-    This was written to support a specific configuration style and should be
-    removed if that style is no longer useful.
-    """
-    return (
-        tune.sample_from(lambda _: random.choice(tune_opts))
-        if not hasattr(args, argparse_attr) or getattr(args, argparse_attr) is None
-        else getattr(args, argparse_attr)
-    )
+# The duplication of tune.sample_from(...) is intentional. This makes it easier
+# to change sampling strategy in the future for certain parameters.
+TUNE_DEFAULT_CONFIG = {
+    "discount": tune.sample_from(
+        lambda _: random.choice([0.9, 0.99, 0.995, 0.999, 0.9995, 0.9999])
+    ),
+    "epsilon": tune.sample_from(lambda _: random.choice([0.01, 0.05, 0.08, 0.1])),
+    "lr": tune.sample_from(lambda _: random.choice([0.01, 0.05, 0.1, 0.5, 1.0])),
+    "batch_size": tune.sample_from(
+        lambda _: random.choice([32, 64, 128, 256, 512, 1024, 2048])
+    ),
+    "clipping": tune.sample_from(lambda _: random.choice([0.1, 0.2, 0.5])),
+    "entropy_bonus": tune.sample_from(lambda _: random.choice([0.0, 0.01, 0.05])),
+}
 
 
 def tune_config(args: argparse.Namespace) -> Dict[str, Any]:
-    return {
-        "discount": config_from_argparse(
-            args,
-            argparse_attr="discount",
-            tune_opts=[0.9, 0.99, 0.995, 0.999, 0.9995, 0.9999],
-        ),
-        "epsilon": config_from_argparse(
-            args, argparse_attr="epsilon", tune_opts=[0.01, 0.05, 0.08, 0.1]
-        ),
-        "lr": config_from_argparse(
-            args, argparse_attr="lr", tune_opts=[0.01, 0.05, 0.1, 0.5, 1.0]
-        ),
-        "batch_size": config_from_argparse(
-            args,
-            argparse_attr="batch_size",
-            tune_opts=[32, 64, 128, 256, 512, 1024, 2048],
-        ),
-        "clipping": config_from_argparse(
-            args, argparse_attr="clipping", tune_opts=[0.1, 0.2, 0.5]
-        ),
-        "entropy_bonus": config_from_argparse(
-            args, argparse_attr="entropy_bonus", tune_opts=[0.0, 0.01, 0.05]
-        ),
-        "num_samples": 1,
-    }
+    """Helper function to set the `config` for Ray Tune. This has to be passed
+    separately from argparse to conform to Tune's API.
+
+    Usage:
+
+    Say the command line is `python3 main.py -t lr -t discount`. Then this will
+    extract the `lr` and `discount` keys from `TUNE_DEFAULT_CONFIG`.
+    """
+    config = deepcopy(TUNE_REQUIRED_CONFIG)
+
+    if args.tune is not None:
+        for tunable_param in args.tune:
+            config[tunable_param] = TUNE_DEFAULT_CONFIG[tunable_param]
+    return config
